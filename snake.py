61cd976e55c407ac4787f2c2f8af99d99ec5425f
a-art <a.al.artamonova@yandex.ru>
2023-01-10 18:19:19 +0400

    v.5

--- snake.py
+++ snake.py
@@ -1,10 +1,10 @@
-# игра доделана
-import pygame
 import sys
 import random
+import pygame
 import pygame_menu
 
 pygame.init()
+
 # цветa
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
@@ -13,29 +13,41 @@ WHITE = (255, 255, 255)
 GREEN = (217, 245, 181)
 SNAKE_COLOR = (84, 140, 11)
 TITLE_COL = (227, 240, 211)
+
 # отступы
 INDENT = 1
 TITLE_INDENT = 70
+
 # размер одного маленького квадратика
 LEN_SQ = 20
 NUM_SQ = 20
+
 # расчитываем размер экрана
 size = [LEN_SQ * NUM_SQ + 2 * LEN_SQ + INDENT * NUM_SQ,
         LEN_SQ * NUM_SQ + 2 * LEN_SQ + INDENT * NUM_SQ + TITLE_INDENT]
 print(size)
+
 # создаем окно с размером size
 screen = pygame.display.set_mode(size)
+
 # даём название окну
-pygame.display.set_caption('Snake game')
+NAME = 'Snake game'
+pygame.display.set_caption(NAME)
+
 # делаем другую иконку окна
 img = pygame.image.load('snake_photo.png')
 pygame.display.set_icon(img)
 timer = pygame.time.Clock()
+# шрифт
 comicsansms = pygame.font.SysFont('comicsansms', 32)
+
+# фон для меню
+fon_img = pygame.image.load('fon.png')
 
 
 # класс для змейки
 class Snake:
+    # с координатами поля
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -50,112 +62,157 @@ class Snake:
 
 
 # функция для рисования квадратиков (как для поля, так и для змейки)
-
 def add_new_block(color, row, col):
     pygame.draw.rect(screen, color, [LEN_SQ + col * LEN_SQ + INDENT * (col + 1),
                                      TITLE_INDENT + LEN_SQ + row * LEN_SQ + INDENT * (row + 1),
                                      LEN_SQ, LEN_SQ])
 
 
-# функция для получения рандомной клетки для яблочка
-def new_blocks():
-    x = random.randint(0, NUM_SQ - 1)
-    y = random.randint(0, NUM_SQ - 1)
-    new_block = Snake(x, y)
-    while new_block in snake_pos:
-        new_block.x = random.randint(0, NUM_SQ - 1)
-        new_block.y = random.randint(0, NUM_SQ - 1)
-    return new_block
+def apple_square():
+    # функция для получения рандомной клетки для яблочка
+    def new_blocks():
+        x = random.randint(0, NUM_SQ - 1)
+        y = random.randint(0, NUM_SQ - 1)
+        new_block = Snake(x, y)
+        while new_block in snake_pos:
+            new_block.x = random.randint(0, NUM_SQ - 1)
+            new_block.y = random.randint(0, NUM_SQ - 1)
+        return new_block
+
+    # положение змейки
+    snake_pos = [Snake(9, 8), Snake(9, 9), Snake(9, 10)]
+
+    # положение яблочка
+    apple = new_blocks()
+
+    # смещения
+    d_row = 0
+    d_col = 1
+    del_row = 0
+    del_col = 1
+
+    # очки
+    score = 0
+
+    # скорость змейки
+    speed = 1
+
+    # игровой цикл while
+    while True:
+        for eve in pygame.event.get():
+
+            # обработка выхода
+            if eve.type == pygame.QUIT:
+                print('выход')
+                pygame.quit()
+                sys.exit()
+
+            elif eve.type == pygame.KEYDOWN:
+                # обработка нажатий клавиш-стрелок
+                if eve.key == pygame.K_UP and d_col != 0:
+                    del_row = -1
+                    del_col = 0
+                elif eve.key == pygame.K_DOWN and d_col != 0:
+                    del_row = 1
+                    del_col = 0
+                elif eve.key == pygame.K_LEFT and d_row != 0:
+                    del_row = 0
+                    del_col = -1
+                elif eve.key == pygame.K_RIGHT and d_row != 0:
+                    del_row = 0
+                    del_col = 1
+
+                # выход на ESCAPE
+                elif eve.key == pygame.K_ESCAPE:
+                    print('выход escape')
+                    pygame.quit()
+                    sys.exit()
+
+        screen.fill(MAIN_COLOR)
+
+        # шапочка
+        pygame.draw.rect(screen, TITLE_COL, [0, 0, size[0], TITLE_INDENT])
+
+        # отображение текста со скоростью и резутьтатом на шапочке
+        text_score = comicsansms.render(f'Счёт: {score}', True, BLACK)
+        text_speed = comicsansms.render(f'Скорость: {speed}', True, BLACK)
+        screen.blit(text_score, (LEN_SQ, LEN_SQ))
+        screen.blit(text_speed, (LEN_SQ + 250, LEN_SQ))
+
+        # рисуем игровое поле
+        for row in range(NUM_SQ):
+            for col in range(NUM_SQ):
+                # проверка на четность => цвет квадратика
+                if (col + row) % 2 == 0:
+                    color = GREEN
+                else:
+                    color = WHITE
+
+                add_new_block(color, row, col)
+
+        # движение змейки и ее отрисовка
+        # тк голова - последний эдемент списка
+        head = snake_pos[-1]
+        if not head.correct_coords():
+            print('змейка врезалась в стенку')
+            break
+            # pygame.quit()
+            # sys.exit()
+        # отрисовка яблочка
+        add_new_block(RED, apple.x, apple.y)
+        for block in snake_pos:
+            add_new_block(SNAKE_COLOR, block.x, block.y)
+
+        pygame.display.flip()
+
+        # встреча головы змейки и яблока:
+        if apple == head:
+            score += 1
+            speed = score // 4 + 1
+            snake_pos.append(apple)
+            apple = new_blocks()
+
+        # для поедания змейки самой себя
+        d_row = del_row
+        d_col = del_col
+
+        new_head = Snake(head.x + d_row, head.y + d_col)
+
+        if new_head in snake_pos:
+            print('змейка врезалась в себя')
+            break
+            # pygame.quit()
+            # sys.exit()
+        snake_pos.append(new_head)
+        snake_pos.pop(0)
+
+        timer.tick(4 + speed)
 
 
-# положение змейки
-snake_pos = [Snake(9, 8), Snake(9, 9), Snake(9, 10)]
-# положение яблочка
-apple = new_blocks()
-# смещения
-d_row = 0
-d_col = 1
-del_row = 0
-del_col = 1
-# очки
-score = 0
-# скорость змейки
-speed = 1
-# игровой цикл while
+# встроенные темы в модуле menu
+main_theme = pygame_menu.themes.THEME_GREEN.copy()
+main_theme.set_background_color_opacity(0.7)
+
+# экран с меню
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                        theme=main_theme)
+menu.add.text_input('Имя :', default='Игрок 1')
+menu.add.button('Играть', apple_square)
+menu.add.button('Выход', pygame_menu.events.EXIT)
+
+# цикл для меню
+
 while True:
-    for eve in pygame.event.get():
-        # обработка выхода
-        if eve.type == pygame.QUIT:
-            print('выход')
-            pygame.quit()
-            sys.exit()
-        elif eve.type == pygame.KEYDOWN:
-            # обработка нажатий клавиш-стрелок
-            if eve.key == pygame.K_UP and d_col != 0:
-                del_row = -1
-                del_col = 0
-            elif eve.key == pygame.K_DOWN and d_col != 0:
-                del_row = 1
-                del_col = 0
-            elif eve.key == pygame.K_LEFT and d_row != 0:
-                del_row = 0
-                del_col = -1
-            elif eve.key == pygame.K_RIGHT and d_row != 0:
-                del_row = 0
-                del_col = 1
 
-    screen.fill(MAIN_COLOR)
-    # шапочка
-    pygame.draw.rect(screen, TITLE_COL, [0, 0, size[0], TITLE_INDENT])
+    screen.blit(fon_img, (0, 0))
 
-    # отображение текста со скоростью и резутьтатом на шапочке
-    text_total = comicsansms.render(f'Счёт: {score}', True, BLACK)
-    text_speed = comicsansms.render(f'Скорость: {speed}', True, BLACK)
-    screen.blit(text_total, (LEN_SQ, LEN_SQ))
-    screen.blit(text_speed, (LEN_SQ + 250, LEN_SQ))
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            exit()
 
-    # рисуем игровое поле
-    for row in range(NUM_SQ):
-        for col in range(NUM_SQ):
-            # проверка на четность => цвет квадратика
-            if (col + row) % 2 == 0:
-                color = GREEN
-            else:
-                color = WHITE
+    if menu.is_enabled():
+        menu.update(events)
+        menu.draw(screen)
 
-            add_new_block(color, row, col)
-
-    # движение змейки и ее отрисовка
-    # тк голова - последний эдемент списка
-    head = snake_pos[-1]
-    if not head.correct_coords():
-        print('змейка врезалась в стенку')
-        pygame.quit()
-        sys.exit()
-
-    add_new_block(RED, apple.x, apple.y)
-    for block in snake_pos:
-        add_new_block(SNAKE_COLOR, block.x, block.y)
-
-    pygame.display.flip()
-
-    # встреча головы змейки и яблока:
-    if apple == head:
-        score += 1
-        speed = score // 4 + 1
-        snake_pos.append(apple)
-        apple = new_blocks()
-
-    d_row = del_row
-    d_col = del_col
-
-    new_head = Snake(head.x + d_row, head.y + d_col)
-
-    if new_head in snake_pos:
-        print('змейка врезалась в себя')
-        pygame.quit()
-        sys.exit()
-    snake_pos.append(new_head)
-    snake_pos.pop(0)
-
-    timer.tick(4 + speed)
+    pygame.display.update()
