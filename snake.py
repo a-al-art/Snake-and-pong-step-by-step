82c8ddb79b0e62769a45810cfa6549f042f5af10
a-art <a.al.artamonova@yandex.ru>
2023-01-10 18:09:10 +0400

    v.2

--- snake.py
+++ snake.py
@@ -1,10 +1,11 @@
-# создали поле
+# создали змейку
 import pygame
 
 # цветa
 MAIN_COLOR = (185, 247, 104)
 WHITE = (255, 255, 255)
 GREEN = (217, 245, 181)
+SNAKE_COLOR = (84, 140, 11)
 TITLE_COL = (227, 240, 211)
 # отступы
 INDENT = 1
@@ -20,10 +21,27 @@ size = [LEN_SQ * NUM_SQ + 2 * LEN_SQ + INDENT * NUM_SQ,
 screen = pygame.display.set_mode(size)
 # даём название окну
 pygame.display.set_caption('Snake game')
+
+
+# функция для рисования квадратиков (как для поля, так и для змейки)
+def add_new_block(color, row, col):
+    pygame.draw.rect(screen, color, [LEN_SQ + col * LEN_SQ + INDENT * (col + 1),
+                                     TITLE_INDENT + LEN_SQ + row * LEN_SQ + INDENT * (row + 1),
+                                     LEN_SQ, LEN_SQ])
+
+
+class Snake:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+snake_pos = [Snake(9, 9)]
+
 # игровой цикл while
 while True:
-    for eve in pygame.event.get():
-        if eve.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             print('выход')
             pygame.quit()
 
@@ -38,8 +56,9 @@ while True:
                 color = GREEN
             else:
                 color = WHITE
-            pygame.draw.rect(screen, color, (LEN_SQ + col * LEN_SQ + INDENT * (col + 1),
-                                             TITLE_INDENT + LEN_SQ + row * LEN_SQ + INDENT * (row + 1),
-                                             LEN_SQ, LEN_SQ))
 
+            add_new_block(color, row, col)
+
+    for block in snake_pos:
+        add_new_block(SNAKE_COLOR, block.x, block.y)
     pygame.display.flip()
